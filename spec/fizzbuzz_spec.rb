b2c287c5dab662c2cadcde42011b745b8f6e327e
Jonny Moore <jonnymoore121212@gmail.com>
2016-07-04 17:27:45 +0100

    9: Completed kata using returns

---
+++ spec/fizzbuzz_spec.rb
@@ -0,0 +1,16 @@
+require 'fizzbuzz'
+
+describe 'fizzbuzz' do
+  it 'returns "fizzbuzz" for multiples of 3 and 5' do
+    expect(fizzbuzz(15)).to eq "fizzbuzz"
+  end
+  it 'returns "fizz" for multiples of 3, but not 5' do
+    expect(fizzbuzz(6)).to eq "fizz"
+  end
+  it 'returns "buzz" for multiples of 5, but not 3' do
+    expect(fizzbuzz(10)).to eq "buzz"
+  end
+  it 'returns input number for non multiples of 3 or 5' do
+    expect(fizzbuzz(1)).to eq 1
+  end
+end
