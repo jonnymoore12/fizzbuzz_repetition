b39eb90dbac5262de4fb07f34ab1dbf4e3832972
Jonny Moore <jonnymoore121212@gmail.com>
2016-07-04 16:34:14 +0100

    3: Deleted file contents

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,12 +1,1 @@
-def fizzbuzz(number)
-  case
-  when (number % 15 == 0)
-    "fizzbuzz"
-  when (number % 3 == 0)
-    "fizz"
-  when (number % 5 == 0)
-    "buzz"
-  else
-    number
-  end
-end
+
