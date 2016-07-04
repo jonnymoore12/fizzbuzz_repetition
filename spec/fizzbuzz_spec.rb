ec3d3bded48737c764295dbc92d2e0054828c256
Jonny Moore <jonnymoore121212@gmail.com>
2016-07-04 16:24:14 +0100

    1: Completed kata

---
+++ spec/fizzbuzz_spec.rb
@@ -0,0 +1,16 @@
+require 'fizzbuzz'
+
+describe "fizzbuzz" do
+  it 'returns "fizz" when passed a multiple of 3' do
+    expect(fizzbuzz(3)).to eq "fizz"
+  end
+  it 'returns "buzz" when passed a multiple of 5' do
+    expect(fizzbuzz(5)).to eq "buzz"
+  end
+  it 'returns "fizzbuzz" when passed a multiple of 3 and 5' do
+    expect(fizzbuzz(15)).to eq "fizzbuzz"
+  end
+  it "returns itself when passed a non multiple of 3 or 5" do
+    expect(fizzbuzz(2)).to eq 2
+  end
+end
